bb39c9619ac628b12ccbb44f1b9cd374deaa45a0
AlpBayram <57071283+alpbayram@users.noreply.github.com>
2025-03-08 15:43:34 +0300

    hamburger menu sorunu

--- scripts/app.js
+++ scripts/app.js
@@ -9,26 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const style2 = getComputedStyle(document.querySelector(".icon-x")).display;
     btn.addEventListener("click", menuOpen);
     function menuOpen() {
+        style = getComputedStyle(document.querySelector(".sidebar")).display;
         if (style == "none") {
-            sidebar.style.display="grid"
-           
+            sidebar.style.display="grid"           
             sidebar.style.gridColumn = "1/2";
             sidebar.style.gridRow = "1/-1";
             
             sidebar.style.zIndex="3"
             sidebar.style.width="11rem"
             iconX.style.display="flex"
-            style = getComputedStyle(document.querySelector(".sidebar")).display;
+            
         }
     }
     
     iconX.addEventListener("click", menuClose);
     function menuClose() {
-       
+        style = getComputedStyle(document.querySelector(".sidebar")).display;
         if (style == "grid") {
             sidebar.style.display="none"
             console.log("giriyor")
-            style = getComputedStyle(document.querySelector(".sidebar")).display;
+            
         }
     }
   });
